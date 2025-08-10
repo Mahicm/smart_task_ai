82f15ff212399d0a63ea0ce3e24d81c70276dd14
Mahicm <mahichinu02@gmail.com>
2025-08-10 21:55:21 +0530

    worked on Frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1 className='container mx-auto text-center text-2xl font-bold mt-10 bg-blue-500 text-white p-5 rounded-lg shadow-lg'>
+      Hello, Smart Task AI!
+      
+     </h1>
+    </div>
+  );
+}
+
+export default App;
